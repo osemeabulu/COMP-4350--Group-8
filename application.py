8ee56cecc7a99f0638ddaf25a062a75fc7917ea3
Rory <rory@GLaDOS.(none)>
2013-02-05 20:37:28 -0600

    Revised the web page to use bootstrap and added a gitignore file.

--- application.py
+++ application.py
@@ -6,16 +6,10 @@ from flask import *
 # the callable be named 'application' by default.
 application = Flask(__name__)
 
-# Setup the root route of the website, and render the 'index.html' template
 @application.route("/")
-def default():
-	#display welcome page
-	return render_template('index.html')
-
-@application.route("/page2")
-def page2():
-	return render_template('page2.html')
+def index():
+    return render_template('index.html');
 
 if __name__ == '__main__':
-	application.debug = True
-	application.run(host='0.0.0.0')
+    application.debug = True
+    application.run(host='0.0.0.0')

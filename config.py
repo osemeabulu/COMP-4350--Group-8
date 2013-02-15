2635aa9e8498a1da2394195b20e3c6ea2fb20c05
Rory <rory@blue.(none)>
2013-02-15 13:15:48 -0600

    Changed config db location

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-import os
-_basedir = os.path.abspath(os.path.dirname(__file__))
+#import os
+#_basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/cris.db'
 DATABASE_CONNECT_OPTIONS = {}

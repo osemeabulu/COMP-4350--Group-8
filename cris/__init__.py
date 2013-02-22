27cdb6f170d5bd2d9c3bd7abb692b0b23922fe6b
Noel <umganoti@cc.umanitoba.ca>
2013-02-22 14:07:08 -0600

    Added more course descriptions.

--- cris/__init__.py
+++ cris/__init__.py
@@ -54,19 +54,35 @@ aut = Course('Comp3030', 'Automata Theory and Formal Languages', 'Calendar Descr
 'turing machines. Applications: lexical analysis, text editing, machine design, syntax' +
 'analysis, parser generation.' + 
 'Prerequisites: COMP 2080 and COMP 2140. This course is a prerequisite for: COMP 4310')
-#aa = Course('Comp3170', 'Analysis of Algorithms and Data Structures')
-#ai = Course('Comp3190', 'Artificial Intelligence')
-#se1 = Course('Comp3350', 'Software Engineering 1')
-#os1 = Course('Comp3430', 'Operating Systems 1')
-#se2 = Course('Comp4350', 'Software Engineering 2')
+
+aa = Course('Comp3170', 'Analysis of Algorithms and Data Structures', 'Calendar Description: Fundamental Algorithms for sorting, searching, storage' +
+'management, graphs, databases and computational geometry.  Correctness and Analysis' + 
+'of those Algorithms using specific data structures.  An introduction to lower bounds and' + 
+'intractability. Prerequisites: COMP 2080 and COMP 2140. This course is a prerequisite for: COMP 4340 and COMP 4420')
+
+ai = Course('Comp3190', 'Artificial Intelligence', 'Calendar Description: Principles of artificial intelligence; problem solving, knowledge' +
+'representation and manipulation; the application of these principles to the solution of hard' + 
+'problems. Prerequisite: COMP 2140. This course is a prerequisite for: COMP 4190, COMP 4200 and COMP 4360.' )
+
+se1 = Course('Comp3350', 'Software Engineering 1', 'Calendar Description: Introduction to software engineering.  Software life cycle' +
+'models, system and software requirements analysis, specifications, software design,' + 
+'testing, and maintenance, software quality. Prerequisite: COMP 2150. This course is a prerequisite for: COMP 4050, COMP 4350 and COMP 4560' )
+
+os1 = Course('Comp3430', 'Operating Systems 1', 'Calendar Description: Operating systems, their design, implementation, and usage (Lab ' +
+'required).  Prerequisite: COMP 2140 and COMP 2280 Recommended: COMP 2160 ' +
+'This course is a prerequisite for: COMP 4430, COMP 4510, COMP 4550 and COMP 4580' )
+
+se2 = Course('Comp4350', 'Software Engineering 2', 'Calendar Description:  Advanced treatment of software development methods. Topics' + 
+'will be selected from requirements gathering, design methodologies, prototyping,' + 
+'software verification and validation.  Prerequisite: COMP 3350.' )
 
 db.session.add(oo)
 db.session.add(aut)
-#db.session.add(aa)
-#db.session.add(ai)
-#db.session.add(se1)
-#db.session.add(os1)
-#db.session.add(se2)
+db.session.add(aa)
+db.session.add(ai)
+db.session.add(se1)
+db.session.add(os1)
+db.session.add(se2)
 
 db.session.commit()
 

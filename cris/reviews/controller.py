4f30d7c5803b7cd8f8a03ec119d6b929c4a550cb
Rory <rory@blue.(none)>
2013-02-26 21:08:15 -0600

    added review creation to reviews controller and added a sample review to the __init__ file.

--- cris/reviews/controller.py
+++ cris/reviews/controller.py
@@ -1,9 +1,24 @@
-from flask import Blueprint, jsonify, render_tamplate, request
+from flask import Blueprint, jsonify, render_template, request
+from cris import db
 from model import Review
 
 mod = Blueprint('reviews', __name__, url_prefix='/reviews')
 
-@mod.rout('/_query_by_course')
+@mod.route('/_submit_review')
+def submit_review():
+	data = request.json;
+	review = Review(
+			data['cid'],
+			data['rscr'],
+			data['rdesc'],
+			data['rvote'])
+
+	db.session.add(review)
+	db.session.commit()
+
+	flash('Review received and processed')
+
+@mod.route('/_query_by_course')
 def query_by_course():
 	key = request.args.get('key', '')
 

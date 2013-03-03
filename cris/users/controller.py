1056525ba5352535b7862ef78f291062c801ec73
Ian <ian@ubuntu.(none)>
2013-03-02 22:02:53 -0600

    Register Page added

--- cris/users/controller.py
+++ cris/users/controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify, render_template, request, flash
+from flask import Blueprint, jsonify, render_template, redirect, url_for, request, flash, session
 from cris import db
+from sqlalchemy.exc import IntegrityError
 from model import User
 
 mod = Blueprint('users', __name__, url_prefix='/users')
@@ -7,6 +8,26 @@ mod = Blueprint('users', __name__, url_prefix='/users')
 @mod.route('/')
 def index():
 	return render_template('users/users.html')
+  
+@mod.route('/register', methods = ['GET', 'POST'])
+def register():	
+	error = None
+	if request.method == 'POST':
+		if request.form['password'] == request.form['password_c']:
+			try:
+				db.session.add(User(request.form['username'], request.form['password']))
+				db.session.commit()
+				session['username'] = request.form['username'] #login
+				return redirect(url_for('index'))
+			except IntegrityError:
+				error = "User with that name already exists"
+		else:
+			error = "Passwords don't match"
+
+	return render_template('users/register.html', error=error)
+   
+		
+
 
 @mod.route('/_query')
 def query():

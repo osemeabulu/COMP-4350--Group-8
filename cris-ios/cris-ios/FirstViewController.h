5fb10f1616c4b3472f9c9b240522a8a6d67c528b
Scott Hofer <umhofers@vole.cs.umanitoba.ca>
2013-03-08 15:16:34 -0600

    Adding in base iOS app.

---
+++ cris-ios/cris-ios/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  cris-ios
+//
+//  Created by Scott Hofer on 2013-03-08.
+//  Copyright (c) 2013 Scott Hofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ cris-ios/cris-iosTests/cris_iosTests.h
@@ -0,0 +1,13 @@
+//
+//  cris_iosTests.h
+//  cris-iosTests
+//
+//  Created by Scott Hofer on 2013-03-08.
+//  Copyright (c) 2013 Scott Hofer. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface cris_iosTests : SenTestCase
+
+@end

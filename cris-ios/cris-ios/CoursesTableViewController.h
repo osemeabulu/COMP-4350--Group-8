8500228ab46520a88d8a2c695c2ff0e743f6cb33
umfinne2 <umfinne2@cc.umanitoba.ca>
2013-03-09 14:23:11 -0800

    Added a CourseList ViewController that currently displays a hardcoded list of courses.  Next will be to query the server for json objects and populate list from that.

---
+++ cris-ios/cris-ios/CoursesTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  CoursesTableViewController.h
+//  cris-ios
+//
+//  Created by Finn Wake on 2013-03-09.
+//  Copyright (c) 2013 Scott Hofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoursesTableViewController : UITableViewController
+
+
+@property (strong, nonatomic) IBOutlet UITableView *courseList;
+
+@end

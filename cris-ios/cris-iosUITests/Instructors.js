b8a0c6ae64144c066cadff304183d3e1af195d1d
ian <ian@ians-Mac.local>
2013-03-26 19:50:21 -0500

    (ios)Added ui test for instructors

--- cris-ios/cris-iosUITests/Instructors.js
+++ cris-ios/cris-iosUITests/Instructors.js
@@ -1,5 +1,4 @@
-
 var target = UIATarget.localTarget();
 target.frontMostApp().tabBar().buttons()["Instructors"].tap();
 target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["John Braico"].tap();
-
+target.frontMostApp().mainWindow().tableViews()["Empty list"].cells()["Comp2150 - Object Orientation"].tap();
